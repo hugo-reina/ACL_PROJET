457077621f596067da8e1b0400fb638c79411f51
hugoreina <hugoreina.hr@gmail.com>
2023-10-10 20:27:42 +0200

    ajout

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.example.acl_projet {
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
 
     opens com.example.acl_projet to javafx.fxml;
     exports com.example.acl_projet;
